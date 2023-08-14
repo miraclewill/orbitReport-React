2a04f02f50846a9a94d0e09a4e9963b9590f4b48
Miracle Williams <miraclewilliams.phila@gmail.com>
2023-08-13 20:31:51 -0400

    fixed changes again

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,17 +1,18 @@
 import satData from "./satData";
 import "./styling.css";
 
-
-const Buttons = ({filterByType, setSat, displaySats}) => {
-    <div className="flex-container">
-      {displaySats.map((sat, id) => {
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <button onClick={() => filterByType(sat)} key={id}>
-      {sat} Orbit
-    </button>
-  );
-})}
-    <button onClick={() => setSat(satData)}>All Orbits</button>
+    // className optional if applying stylings - else plain <div>
+    <div className="flex-container"> 
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
