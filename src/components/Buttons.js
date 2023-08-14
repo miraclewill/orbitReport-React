ee0bbb8aa2a8db265b629bdc8908ee3ce7920d87
Miracle Williams <miraclewilliams.phila@gmail.com>
2023-08-13 20:10:35 -0400

    made changes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,18 @@
-const Buttons = () => {
+import satData from "./satData";
+import "./styling.css";
+
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
   );
+})}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+      </div>
 };
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,30 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
+        {sat.map((id, data) => {
+          return (
         <tr>
-          <td>Row Data TBD</td>
+           key = {id}
+          <td>{data.name}</td>
+          <td>{data.typeOfSatellites}</td>
+          <td>{data.launchDates}</td>
+          <td>
+            {data.operational ? "Active" : "Inactive"}
+          </td>
         </tr>
+          );
+          })}
         </tbody>
       </table>
   );
